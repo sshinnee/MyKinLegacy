05b189c1be140ceac76cc3a61650c08a75740833
sshinnee <samantha_wong@tech.gov.sg>
2024-01-07 21:41:00 +0800

    add starter LoginView

--- MyKinLegacy/MyKinLegacyApp.swift
+++ MyKinLegacy/MyKinLegacyApp.swift
@@ -13,8 +13,7 @@ struct MyKinLegacyApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            LoginView()
         }
     }
 }
---
+++ MyKinLegacy/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  MyKinLegacy
+//
+//  Created by shinnee on 7/1/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
