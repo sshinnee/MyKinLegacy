be629c97b543f2ddeb740d8964e1f7a6e2aa896e
sshinnee <samantha_wong@tech.gov.sg>
2024-01-07 22:14:22 +0800

    update StarterView with TextField (allows input)

--- MyKinLegacy/Views/StarterView.swift
+++ MyKinLegacy/Views/StarterView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct StarterView: View {
+    @State private var uin = String()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Please enter the NRIC/FIN of your loved one", text: $uin)
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
